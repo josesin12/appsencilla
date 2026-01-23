d18f44d54b7e8f81b371823ecb0a28053cd88d55
jose_manuel <jkanahua@unsa.edu.pe>
2026-01-22 20:35:13 -0500

    Add scripts for Render deployment

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 
-# Ejecutar Maven Wrapper
-./mvnw clean package
+# Ejecutar la app Spring Boot
+java -jar target/*.jar
